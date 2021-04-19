e46c7ddc83911fb32526d4cbc833f99e3d1c0864
Diego Lima <diesan_l@yahoo.com.br>
2021-04-19 11:38:22 -0300

    added comments

--- debug-de-codigo/src/main/java/one/innovation/digital/Programa.java
+++ debug-de-codigo/src/main/java/one/innovation/digital/Programa.java
@@ -6,7 +6,7 @@ import one.innovation.digital.pessoa.Pessoa;
 public class Programa {
 
     public static void main(String[] args) {
-        final Pessoa pessoa = new Pessoa("André", 1.9, 100.00);
+        final Pessoa pessoa = new Pessoa("Diego", 1.77, 85.00);
 
         final var calculadorDeImc = new CalculadorDeImc();
         final var imc = calculadorDeImc.calcula(pessoa);
